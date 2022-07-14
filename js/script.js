f81252e2a90eda72eef94ef2ee13aeaaf1f353f3
justIsal <109224556+justIsal@users.noreply.github.com>
2022-07-14 16:17:22 +0700

    remove comment

--- js/script.js
+++ js/script.js
@@ -8,27 +8,3 @@ window.addEventListener('scroll', function() {
 atas.addEventListener('click', function() {
     return location.href='#about';
 });
-
-
-// const nav = document.querySelector('nav'),
-//       navList = nav.querySelectorAll('li'),
-//       totalNavList = navList.length,
-//       allSection = document.querySelectorAll('.section'),
-//       totalSections = allSection.length;
-//       for(let i=0; i<totalNavList; i++) {
-//         const a = navList[i].querySelector("a");
-//         a.addEventListener("click", function () {
-//             for(let j=0; j<totalNavList; j++) {
-//                 navList[j].querySelector("a").classList.remove("active");
-//             }
-//             this.classList.add("active")
-//             showSection(this);
-//         })
-//       }
-//       function showSection(element) {
-//         for(let i=0; i<totalSections; i++) {
-//             allSection[i].classList.remove("active");
-//         }
-//         const target = element.getAttribute("href").split("#")[1];
-//         document.querySelector("#" + target).classList.add("active");
-//       }
